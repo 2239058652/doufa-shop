cae07b454f5e4b37ce41b41bcdb20b925b2c413e
xiao ding <2239058652@qq.com>
2024-12-21 11:43:58 +0800

    引入ant-design-vue

--- src/components/TopBar/TopBar.vue
+++ src/components/TopBar/TopBar.vue
@@ -1,10 +1,25 @@
 <template>
   <div>
-    顶部导航
+    <a-float-button type="primary" :style="{
+      right: '24px',
+    }">
+      <template #icon>
+        <QuestionCircleOutlined />
+      </template>
+    </a-float-button>
+
+    <a-float-button type="default" :style="{
+      right: '94px',
+    }">
+      <template #icon>
+        <QuestionCircleOutlined />
+      </template>
+    </a-float-button>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { QuestionCircleOutlined } from '@ant-design/icons-vue';
 
 </script>
 
