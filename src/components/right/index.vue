6277153963b3f7d9ad4dcf1df571819fef3792ce
xiao ding <2239058652@qq.com>
2024-12-26 11:04:09 +0800

    添加右侧菜单，实现回到顶部，添加相应右侧菜单图片

--- src/components/right/index.vue
+++ src/components/right/index.vue
@@ -1,32 +1,32 @@
 <template>
-  <div class="fixed">
-    <div class="fixed_item">
-      <img src="../../assets/image/logo.png" />
+  <div class="right-menu">
+    <div class="r-menu-item">
+      <img src="../../assets/image/fixed_live.png" />
       <span>我的收藏</span>
     </div>
-    <div class="fixed_item">
-      <img src="../../assets/image/logo.png" />
+    <div class="r-menu-item">
+      <img src="../../assets/image/fixed_shopcar.png" />
       <span>购物车</span>
     </div>
-    <div class="fixed_item">
-      <img src="../../assets/image/logo.png" />
+    <div class="r-menu-item">
+      <img src="../../assets/image/fixed_email.png" />
       <span>站内信</span>
     </div>
-    <a-popover placement="left" :width="230" trigger="hover">
-      <img src="../../assets/image/logo.png" style="width: 200px; height: 200px" />
-      <template #reference>
-        <div class="fixed_item">
-          <img src="../../assets/image/logo.png" />
-          <span>联系我们</span>
-        </div>
+    <a-popover placement="left">
+      <template #content>
+        <img src="../../assets/image/kefu_erweima.png" style="width: 200px; height: 200px" />
       </template>
+      <div class="r-menu-item">
+        <img src="../../assets/image/fixed_waiter.png" />
+        <span>联系我们</span>
+      </div>
     </a-popover>
-    <div class="fixed_item">
-      <img src="../../assets/image/logo.png" />
+    <div class="r-menu-item">
+      <img src="../../assets/image/fixed_feedback.png" />
       <span>投诉反馈</span>
     </div>
-    <div class="fixed_item" @click="goTop">
-      <img src="../../assets/image/logo.png" />
+    <div class="r-menu-item" @click="goTop">
+      <img src="../../assets/image/fixed_toTop.png" />
       <span>返回顶部</span>
     </div>
   </div>
@@ -35,10 +35,14 @@
 <script lang="ts" setup>
 // 固定栏返回顶部按钮
 const goTop = () => {
-  window.scrollTo({
-    top: 0,
-    behavior: 'smooth'
-  })
+  // 因为设置了高度，所以回到顶部会失效，要获取到router-view的dom元素，然后调用scrollTo方法
+  const appElement = document.querySelector('.router-view')
+  if (appElement) {
+    appElement.scrollTo({
+      top: 0,
+      behavior: 'smooth' // 平滑滚动
+    })
+  }
 }
 </script>
 
