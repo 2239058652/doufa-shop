9c0082eae1c6ba4af23302583c73b5bf87279869
xiao ding <2239058652@qq.com>
2025-01-01 11:32:12 +0800

    确认收货信息

--- src/views/PayOrder/PayOrder.vue
+++ src/views/PayOrder/PayOrder.vue
@@ -12,6 +12,49 @@
       <div class="address_sales_info">
         <div class="address_info">
           <div class="address_title">确认收货信息</div>
+          <div class="input_textarea">
+            <a-input type="textarea" :auto-size="{ minRows: 1, maxRows: 3 }" v-model:value="textareaVal"
+              :bordered="false" placeholder="粘贴文本，智能识别收货信息">
+              <template #suffix>
+                <div class="suffix">
+                  <span>快速识别</span>
+                </div>
+              </template>
+            </a-input>
+          </div>
+          <div class="adress_tishi">
+            例：抖发网，13888888888，浙江省 杭州市 上城区 清江路138号，景园家苑，310016
+          </div>
+          <div class="form_row">
+            <a-form ref="formAddressRef" labelAlign="left" :model="formState" name="address" autocomplete="off">
+              <a-row :gutter="16">
+                <a-col :span="8">
+                  <a-form-item label="收货人" name="username"
+                    :rules="[{ required: true, message: 'Please input your username!' }]">
+                    <a-input v-model:value="formState.username" />
+                  </a-form-item>
+                </a-col>
+                <a-col :span="8">
+                  <a-form-item label="联系方式" name="username"
+                    :rules="[{ required: true, message: 'Please input your username!' }]">
+                    <a-input v-model:value="formState.username" />
+                  </a-form-item>
+                </a-col>
+                <a-col :span="8">
+                  <a-form-item label="收货地区" name="username"
+                    :rules="[{ required: true, message: 'Please input your username!' }]">
+                    <a-input v-model:value="formState.username" />
+                  </a-form-item>
+                </a-col>
+                <a-col :span="24">
+                  <a-form-item label="详细地址" name="username"
+                    :rules="[{ required: true, message: 'Please input your username!' }]">
+                    <a-input v-model:value="formState.username" />
+                  </a-form-item>
+                </a-col>
+              </a-row>
+            </a-form>
+          </div>
         </div>
         <div class="sales_info">2</div>
       </div>
@@ -23,11 +66,22 @@
 <script lang="ts" setup>
 import { useRoute } from 'vue-router'
 import { message } from 'ant-design-vue'
+import { reactive, ref } from 'vue'
+import type { FormInstance } from 'ant-design-vue'
 
 
 const [messageApi, contextHolder] = message.useMessage()
 const route = useRoute()
 console.log(route.query, 'route')
+
+const textareaVal = ref('')  // 智能识别信息
+
+const formAddressRef = ref<FormInstance>()
+const formState = reactive<any>({
+  username: '',
+  password: '',
+  remember: true,
+})
 
 </script>
 
