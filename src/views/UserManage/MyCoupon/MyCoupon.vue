8bc94dc483f1d8009e19877eab5e7085a1fb7140
xiao ding <2239058652@qq.com>
2025-01-16 17:01:33 +0800

    a

--- src/views/UserManage/MyCoupon/MyCoupon.vue
+++ src/views/UserManage/MyCoupon/MyCoupon.vue
@@ -1,11 +1,7 @@
 <template>
-  <div>
-    我是我的优惠券页面
-  </div>
+  <div> 我是我的优惠券1页面 </div>
 </template>
 
-<script lang="ts" setup>
-
-</script>
+<script lang="ts" setup></script>
 
 <style scoped></style>
